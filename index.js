8f9e135c5d03f66e92242c2e10d633434c0b1298
Steve "Homeschool" Seney <sdseney508@gmail.com>
2022-02-21 16:46:06 -0500

    adding initial readme generator that is run off index.js

---
+++ index.js
@@ -0,0 +1,139 @@
+// npm inquirer is required to execute the Readme generator
+const inquirer = require('inquirer');
+//fs is already included with NPM but is required to generate the readme file.
+const fs = require('fs');
+const { cpuUsage } = require('process');
+
+// TODO: Create an array of questions for user input
+const readmeInputs = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the Project title?',
+
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide the Project Description?',
+
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'How do you install the Project?',
+
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What are the usage restrictions (if any)?',
+
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'Please enter any other developers that need to be credited / cited',
+
+        },
+        {
+            type: 'input',
+            name: 'contributions',
+            message: 'What are the contribution guidelines?',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What tests have been run on the Project?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your github username.',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Enter your email address.',
+        },
+        {
+            type: 'input',
+            name: 'features',
+            message: 'Describe the Project features.',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message:  'Please select a license.',
+            choices: ["Academic", "Apache", "GNU", "ISC", "MIT", "Mozilla", "Open"],
+
+        },
+    ]);
+
+};
+
+const generateReadme = ({title, description, installation, usage, credit, license, features, contributions, tests, github, email}) =>
+
+`#${title} 
+![badge](https://img.shields.io/badge/license-${license}-brighblue)
+## Description
+${description}
+
+## Table of Contents
+-[Installation](#installation)
+
+-[Usage](#usage)
+
+-[Credits](#credits)
+
+-[License](#license)
+
+-[Badges](#badges)
+
+-[Features](#features)
+
+-[How To Contribute](#how_to_contribute)
+
+-[Tests](#tests)
+
+-[Acknowledgements](#acknowledgements)
+
+
+## Installation
+💾
+${installation}
+## Usage
+💻
+${usage}
+## Credits
+${credit}
+## License
+![badge](https://img.shields.io/badge/license-${license}-blue)
+## Badges
+
+## Features
+${features}
+## How_to_contribute
+${contributions}
+
+## Tests
+${tests}
+
+## Questions
+For questions on usage or functionality i can be reached at:
+Github: [${github}](https://github.com/${github})
+E-mail: ${email}
+`
+
+// TODO: Create a function to write README file
+const init = () => {
+    readmeInputs()
+      // Use writeFileSync method to use promises instead of a callback function
+      .then((answers) => fs.writeFileSync('readme.md', generateReadme(answers)))
+      .then(() => console.log('Successfully generated the intitial readme'))
+      .catch((err) => console.error(err));
+  };
+
+
+init();
---
+++ readme.md
@@ -0,0 +1,49 @@
+#read me test 3 
+![badge](https://img.shields.io/badge/license-MIT-brighblue)
+## Description
+makes a areadme file
+
+## Table of Contents
+-[Installation](#installation)
+
+-[Usage](#usage)
+
+-[Credits](#credits)
+
+-[License](#license)
+
+-[Badges](#badges)
+
+-[Features](#features)
+
+-[How To Contribute](#how_to_contribute)
+
+-[Tests](#tests)
+
+-[Acknowledgements](#acknowledgements)
+
+
+## Installation
+💾
+run node index.js
+## Usage
+💻
+none
+## Credits
+undefined
+## License
+git add -A![badge](https://img.shields.io/badge/license-MIT-blue)
+## Badges
+
+## Features
+it takes in ser inputs and generates a readme file for their application
+## How_to_contribute
+none
+
+## Tests
+ none
+
+## Questions
+For questions on usage or functionality i can be reached at:
+Github: [sdseney508](https://github.com/sdseney508)
+E-mail: undefined
